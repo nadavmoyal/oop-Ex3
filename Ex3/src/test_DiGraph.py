aad8437e013b2d088b10256b3e39051641e91b16
yehonatanbarel <79272744+yehonatanbarel@users.noreply.github.com>
2021-12-26 10:56:39 +0200

    Add files via upload

--- Ex3/src/test_DiGraph.py
+++ Ex3/src/test_DiGraph.py
@@ -5,15 +5,15 @@ from DiGraph import DiGraph
 
 class TestDiGraph(TestCase):
     def test_v_size(self):
-        g = DiGraph()  
+        g = DiGraph()
         for n in range(4):
             g.add_node(n)
-        self.assertEqual(g.v_size(), 4)  
+        self.assertEqual(g.v_size(), 4)
         g.remove_node(2)
-        self.assertEqual(g.v_size(), 3)  
+        self.assertEqual(g.v_size(), 3)
 
     def test_e_size(self):
-        g = DiGraph()  
+        g = DiGraph()
         for n in range(4):
             g.add_node(n)
         g.add_edge(0, 1, 1)
@@ -21,12 +21,12 @@ class TestDiGraph(TestCase):
         g.add_edge(1, 2, 1.3)
         g.add_edge(2, 3, 1.1)
         g.add_edge(1, 3, 1.9)
-        self.assertEqual(g.e_size(), 5)  
+        self.assertEqual(g.e_size(), 5)
         g.remove_edge(1, 3)
-        self.assertEqual(g.e_size(), 4)  
+        self.assertEqual(g.e_size(), 4)
 
     def test_get_mc(self):
-        g = DiGraph()  
+        g = DiGraph()
         for n in range(4):
             g.add_node(n)
         self.assertEqual(g.get_mc(), 4)  # after add 4 node '_mc' will be 4
@@ -34,7 +34,7 @@ class TestDiGraph(TestCase):
         self.assertEqual(g.get_mc(), 5)
 
     def test_add_edge(self):
-        g = DiGraph()  
+        g = DiGraph()
         for n in range(4):
             g.add_node(n)
         g.add_edge(0, 1, 1)
@@ -42,7 +42,7 @@ class TestDiGraph(TestCase):
         g.add_edge(1, 2, 1.3)
         g.add_edge(2, 3, 1.1)
         g.add_edge(1, 3, 1.9)
-        self.assertEqual(g.e_size(), 5)  
+        self.assertEqual(g.e_size(), 5)
 
         edgeWeWnatToSeeInside = {0: {1: 1.1}, 1: {0: 1}, 2: {1: 1.3}, 3: {2: 1.1, 1: 1.9}}
         self.assertDictEqual(g._edgesInside,edgeWeWnatToSeeInside)
@@ -64,7 +64,7 @@ class TestDiGraph(TestCase):
 
 
     def test_add_node(self):
-        g = DiGraph()  
+        g = DiGraph()
         g.add_node(0,(0,1,0)) # remember that in pos is (x,y,z)
         g.add_node(1,(1,0,0)) # remember that in pos is (x,y,z)
         self.assertEqual(g.v_size(),2)
@@ -78,14 +78,14 @@ class TestDiGraph(TestCase):
         g.add_edge(0, 2, 1)
         g.add_edge(0, 3, 1.1)
         g.add_edge(1, 0, 4)
-        self.assertEqual(4, g.e_size()) 
+        self.assertEqual(4, g.e_size())
         g.remove_node(0)
         self.assertEqual(0, g.e_size())  # all edge connect to node 0 so all need to remove
 
 
 
     def test_remove_edge(self):
-        g = DiGraph()  
+        g = DiGraph()
         for n in range(4):
             g.add_node(n)
         g.add_edge(0, 1, 1)
@@ -93,7 +93,7 @@ class TestDiGraph(TestCase):
         g.add_edge(1, 2, 1.3)
         g.add_edge(2, 3, 1.1)
         g.add_edge(1, 3, 1.9)
-        self.assertEqual(g.e_size(), 5)  
+        self.assertEqual(g.e_size(), 5)
         self.assertEqual(g.get_mc(), 9)  # 4 nodes + 5 edges created
         g.remove_edge(2,3)
         self.assertEqual(g.e_size(), 4)  # after del edge (2,3)
@@ -124,7 +124,7 @@ class TestDiGraph(TestCase):
         g.add_edge(2, 3, 1.1)
         g.add_edge(1, 3, 1.9)
 
-        self.assertEqual(g.e_size(), 5)  
+        self.assertEqual(g.e_size(), 5)
 
         edgeWeWnatToSeeInside = {0: {1: 1.1}, 1: {0: 1}, 2: {1: 1.3}, 3: {2: 1.1, 1: 1.9}}
         self.assertDictEqual(g._edgesInside,edgeWeWnatToSeeInside)
@@ -147,7 +147,7 @@ class TestDiGraph(TestCase):
         g.add_edge(2, 3, 1.1)
         g.add_edge(1, 3, 1.9)
 
-        self.assertEqual(g.e_size(), 5)  
+        self.assertEqual(g.e_size(), 5)
 
         edgeWeWnatToSeeOut = {0: {1: 1}, 1: {0: 1.1, 2: 1.3, 3: 1.9}, 2: {3: 1.1}, 3: {}}
         self.assertDictEqual(g._edgesOut, edgeWeWnatToSeeOut)
@@ -158,5 +158,3 @@ class TestDiGraph(TestCase):
 
         edgeWeWnatToSeeOut = {0: {1: 1}, 1: {0: 1.1, 2: 1.3}, 2: {3: 1.1}, 3: {}}
         self.assertDictEqual(g._edgesOut, edgeWeWnatToSeeOut)
-
-
